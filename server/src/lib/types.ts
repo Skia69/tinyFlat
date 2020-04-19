97a15ccbb2d10c5c4e4e03196ef02262d530600a
Skia <Skiastep@gmail.com>
2020-04-19 19:56:09 +0300

    Adding database collections for Bookings, Listings and Users

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,17 +1,18 @@
 import { Collection, ObjectID } from "mongodb";
 
+export interface Booking {
+  _id: ObjectID;
+}
 export interface Listing {
   _id: ObjectID;
-  title: string;
-  image: string;
-  address: string;
-  price: number;
-  numOfGuests: number;
-  numOfBeds: number;
-  numOfBaths: number;
-  rating: number;
+}
+
+export interface User {
+  _id: ObjectID;
 }
 
 export interface Database {
   listings: Collection<Listing>;
+  users: Collection<User>;
+  bookings: Collection<Booking>;
 }
