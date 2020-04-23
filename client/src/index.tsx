a91f26685ccd3ae7ee0a5f2d9159258c22810c31
Skia <Skiastep@gmail.com>
2020-04-23 17:27:13 +0300

    implemented the necessary logIn functionalities

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Home, Host, Listing, Listings, NotFound, User, Login } from './sections';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
+
+import { Home, Host, Listing, Listings, NotFound, User, Login } from './sections';
 import './styles/index.css';
+import { Viewer } from './lib/types';
 
 const client = new ApolloClient({ uri: '/api' });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+
   return (
     <Router>
-      <Switch>
-        <Layout id="app">
-          <Route exact path={'/'} component={Home} />
-          <Route exact path={'/host'} component={Host} />
-          <Route exact path={'/listing/:id'} component={Listing} />
-          <Route exact path={'/listings/:location?'} component={Listings} />
-          <Route exact path={'/login'} component={Login} />
-          <Route exact path={'/user/:id'} component={User} />
-          <Route exact component={NotFound} />
-        </Layout>
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,15 +1,88 @@
-import React from 'react';
-import { Card, Layout, Typography } from 'antd';
+import React, { useEffect, useRef } from 'react';
+import { Card, Layout, Spin, Typography } from 'antd';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import { Redirect } from 'react-router-dom';
 
-// Image Assets
 import googleLogo from './assets/google_logo.jpg';
+import { Viewer } from '../../lib/types';
+import { AuthUrl as AuthUrlData } from '../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl';
+import { AUTH_URL } from '../../lib/graphql/queries/AuthUrl';
+import { LOG_IN } from '../../lib/graphql/mutations/LogIn';
+import {
+  logIn as LogInData,
+  logInVariables,
+} from '../../lib/graphql/mutations/LogIn/__generated__/logIn';
+import { displaySuccessNotification, displayErrorMessage } from '../../lib/utils';
+import { ErrorBanner } from '../../lib/components';
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
-export const Login = () => {
+/* we're not returning anything since we're only interested in updating the Viewer
+ with the data coming from Google api. */
+interface Props {
+  setViewer: (viewer: Viewer) => void;
+}
+export const Login = ({ setViewer }: Props) => {
+  const client = useApolloClient();
+  // capture the "code" received from Google api and send it to the server so that we receive a token.
+  const [logIn, { data: logInData, loading: logInloading, error: logInError }] = useMutation<
+    LogInData,
+    logInVariables
+  >(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+        displaySuccessNotification("You've successfully logged in!");
+      }
+    },
+  });
+  // the way this is used should be done sparingly.
+  const logInRef = useRef(logIn);
+  /* we'll want to run the login mutation the moment our <LogIn /> component is being rendered 
+  and the "code" is available as a query parameter,
+  therefore we'll make use of the useEffect hook and throw in a conditional. */
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    if (code) {
+      logInRef.current({
+        variables: { input: { code } },
+      });
+    }
+  }, []);
+
+  // excute the authUrl so that it sends back a "code" from Google api.
+  const handleAuthorize = async () => {
+    try {
+      const { data } = await client.query<AuthUrlData>({
+        query: AUTH_URL,
+      });
+      window.location.href = data.authUrl;
+    } catch {
+      displayErrorMessage("Sorry! We weren't able to log you in. Please try again later!");
+    }
+  };
+
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+    return <Redirect to={`/user/${viewerId}`} />;
+  }
+
+  if (logInloading) {
+    return (
+      <Content className="log-in">
+        <Spin size="large" tip="Logging you in..." />
+      </Content>
+    );
+  }
+
+  const logInErrorBannerElement = logInError ? (
+    <ErrorBanner description="We weren't able to log you in. Please try again soon." />
+  ) : null;
+
   return (
     <Content className="log-in">
+      {logInErrorBannerElement}
       <Card className="log-in-card">
         <div className="log-in-card__intro">
           <Title level={3} className="log-in-card__intro-title">
@@ -22,7 +95,7 @@ export const Login = () => {
           </Title>
           <Text>Sign in with Google to start booking available rentals!</Text>
         </div>
-        <button className="log-in-card__google-button">
+        <button className="log-in-card__google-button" onClick={handleAuthorize}>
           <img src={googleLogo} alt="Google Logo" className="log-in-card__google-button-logo" />
           <span className="log-in-card__google-button-text">Sign in with Google</span>
         </button>
