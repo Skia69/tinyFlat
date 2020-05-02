4cb958777aa90b761fb580adef1603ac6ee6ccb9
Skia <Skiastep@gmail.com>
2020-05-02 15:22:29 +0300

    Built the Listings page.

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import { RouteComponentProps, Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { Layout, List, Typography } from 'antd';
+import { ListingCard } from '../../lib/components';
+import { LISTINGS } from '../../lib/graphql/queries';
+import {
+  Listings as ListingsData,
+  ListingsVariables,
+} from '../../lib/graphql/queries/Listings/__generated__/Listings';
+import { ListingsFilter } from '../../lib/graphql/globalTypes';
 
-export const Listings = () => {
+const { Content } = Layout;
+const PAGE_LIMIT = 8;
+
+const { Paragraph, Text, Title } = Typography;
+interface MatchParams {
+  location: string;
+}
+
+export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
+  const { loading, data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
+    variables: {
+      location: match.params.location,
+      filter: ListingsFilter.PRICE_LOW_TO_HIGH,
+      limit: PAGE_LIMIT,
+      page: 1,
+    },
+  });
+
+  const listings = data?.listings;
+  const listingsRegion = listings?.region;
+
+  const listingsSectionElement =
+    listings && listings.result.length ? (
+      <List
+        grid={{ gutter: 8, xs: 1, sm: 2, lg: 4 }}
+        dataSource={listings.result}
+        renderItem={(listing) => (
+          <List.Item>
+            <ListingCard listing={listing} />
+          </List.Item>
+        )}
+      />
+    ) : (
+      <div>
+        <Paragraph>
+          It appears that no listings have yet been created for <Text>"{listingsRegion}"</Text>
+        </Paragraph>
+        <Paragraph>
+          Be the first person to create a <Link to="/host">listing in this area</Link>!
+        </Paragraph>
+      </div>
+    );
+
+  const listingsRegionElement = listingsRegion ? (
+    <Title level={3} className="listings__title">
+      Results for "{listingsRegion}"
+    </Title>
+  ) : null;
+
   return (
-    <div>
-      <h2>Listings</h2>
-    </div>
+    <Content className="listings">
+      {listingsRegionElement}
+      {listingsSectionElement}
+    </Content>
   );
 };
